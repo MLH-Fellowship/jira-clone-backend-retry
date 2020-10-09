b96a33a774c3c5712349e6cdff833a27834e075a
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-10-09 10:26:59 -0400

    added routes for user controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "password", team_id: 1)
+User.create(first_name: "Jake", last_name: "Molly", email: "molly@doe.com", password: "password", team_id: 1)
+User.create(first_name: "Archer", last_name: "Bellmont", email: "hanky@doe.com", password: "password", team_id: 1)
+User.create(first_name: "Archer", last_name: "Bellmont", email: "pinky@doe.com", password: "password", team_id: 2)
+User.create(first_name: "Archer", last_name: "Bellmont", email: "tommy@doe.com", password: "password", team_id: 3)
