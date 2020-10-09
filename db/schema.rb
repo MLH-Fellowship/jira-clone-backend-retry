1e65a6f0fcca9b304923c73ce46f5681335ed637
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-10-09 10:48:32 -0400

    added Team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_133625) do
+ActiveRecord::Schema.define(version: 2020_10_09_144659) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
