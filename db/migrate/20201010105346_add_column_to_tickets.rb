f83200b17d3bfb795a79b6152c1e1381031f3a25
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-10-10 07:03:09 -0400

    added user_id fieled to ticket

---
+++ db/migrate/20201010105346_add_column_to_tickets.rb
@@ -0,0 +1,5 @@
+class AddColumnToTickets < ActiveRecord::Migration[6.0]
+  def change
+    add_column :tickets, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_150009) do
+ActiveRecord::Schema.define(version: 2020_10_10_105346) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_10_09_150009) do
     t.date "due_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
