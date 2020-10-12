653979dfb538779f10628948ba982492f0f0ac17
Brandon Potts <ObeliskIsHuge@gmail.com>
2020-10-12 08:33:13 -0400

    hot-fix for ticket creation

--- app/controllers/ticket_controller.rb
+++ app/controllers/ticket_controller.rb
@@ -10,7 +10,11 @@ class TicketController < ApplicationController
 
   # POST /ticket
   def create
-    @ticket = Ticket.create!(ticket_params)
+    puts params[:issue].inspect
+    # TODO the bottom is commented out due to incorrect JSON passing.
+    # TODO it should be fixed long term
+    # @ticket = Ticket.create!(ticket_params)
+    @ticket = Ticket.create!(create_ticket_params)
     json_response(@ticket, :created)
   end
 
@@ -39,6 +43,12 @@ class TicketController < ApplicationController
 
 
   private
+
+  # Allows mass assignment for the issue field
+  # TODO once the issue field is fixed, remove this method
+  def create_ticket_params
+    params[:issue].permit(:project_id, :status_id, :title, :description, :date_posted, :due_date, :user_id)
+  end
 
   # Used to allow mass assignment
   def ticket_params
